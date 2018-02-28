11681d6470991729dee3aea99c7ef4956a4a2728
Alexander Bachmann <a.t.bachmann@gmail.com>
2018-02-28 18:12:48 +0100

    fix windows directory separator in command logger test

--- test/Broadway/Auditing/CommandLoggerTest.php
+++ test/Broadway/Auditing/CommandLoggerTest.php
@@ -95,7 +95,7 @@ class CommandLoggerTest extends TestCase
 
         $this->assertEquals('Yolo', $loggedData['exception']['message']);
         $this->assertEquals('Broadway\Auditing\MyException', $loggedData['exception']['class']);
-        $this->assertStringEndsWith('test/Broadway/Auditing/CommandLoggerTest.php', $loggedData['exception']['file']);
+        $this->assertStringEndsWith(implode(['test', 'Broadway', 'Auditing', 'CommandLoggerTest.php'], DIRECTORY_SEPARATOR), $loggedData['exception']['file']);
     }
 }
 
